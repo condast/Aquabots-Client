1477fd772596f19a68e99ee322e810251f377107
Kees Pieters <info@condast.com>
2018-09-25 14:52:27 +0200

    TinyGPS added

---
+++ Arduino/Vessels/TinyGPS.h
@@ -0,0 +1,34 @@
+#ifndef TinyGPS_h
+#define TinyGPS_h
+
+#define TINY_GPS "TinyGPS"
+#define TINY_GPS_ID "gps.tiny"
+
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+class TinyGPS {
+
+  private:
+    bool complete; //general purpose flag for reading nmea sentences
+    String sentence;
+    double bearing;
+    double speed;
+    double latitude;
+    double longitude;
+    TinyGPSPlus gps;
+    //void sendNMEA();
+    //bool readNMEA( char chr); //used by SerialEvent
+
+  public: TinyGPS(void);
+    void setup();
+    double getBearing();
+    double getBearing( double latFrom, double lonFrom, double latTo, double lonTo );
+    double getDistance( double latFrom, double lonFrom, double latTo, double lonTo );
+    bool wait();//wait for processing of the nmea sentence
+    void loop();
+};
+
+#endif
+
+
--- Arduino/Vessels/Vessels.ino
+++ Arduino/Vessels/Vessels.ino
@@ -1,26 +1,30 @@
 #include <ArduinoJson.h>
 
 #include "WebClient.h"
-#include "SDCard.h"
 #include "Registration.h"
+#include "TinyGPS.h"
 
 #define VESSEL F("Costa")
 #define PASSPHRASE F("HetIsWelGoed")
 #define LATITUDE 51.2
 #define LONGITUDE 4.2
+#define TIME_OUT 300 //msec
 
+SoftwareSerial Serial1(2, 3); // RX, TX
 WebClient webClient;
-SDCard sdcard;
 Registration registration;
+TinyGPS tinyGPS;
 
 void setup() {
   Serial.begin(9600);
   Serial.print(F("Setup Vessel: ")); Serial.println( VESSEL );
   webClient.setup();
   registration.setup();
+  tinyGPS.setup();
 }
 
 void loop() {
   registration.registerVessel( VESSEL, PASSPHRASE, LATITUDE, LONGITUDE );
   delay(1000);
+  tinyGPS.loop();
 }
