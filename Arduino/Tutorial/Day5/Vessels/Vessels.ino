0538bf22789f08b305fe327cf991655fc4e5ee4d
Kees Pieters <info@condast.com>
2019-05-22 08:43:58 +0200

    Added interrupts and logger

--- Arduino/Tutorial/Day5/Vessels/Vessels.ino
+++ Arduino/Tutorial/Day5/Vessels/Vessels.ino
@@ -5,20 +5,26 @@
 #include "TinyGPS.h"
 #include "Vessel.h"
 #include "ServoController.h"
+#include "Interrupts.h"
+#include "Logger.h"
 
 #define VESSEL F("AquaBoat")
 #define PASSPHRASE F("AquaPassphrase")
 #define LATITUDE 51.2
 #define LONGITUDE 4.2
 #define TIME_OUT 3000 //msec
+#define REFRESH 3
 
 //SoftwareSerial Serial1(2, 3); // RX, TX
 static WebClient webClient;
 static Registration registration;
 static TinyGPS gps;
 static Vessel vessel;
+static Interrupts interrupt;
+static Logger logger;
 
 long vesselId;
+int load;
 
 void setup() {
   Serial.begin(9600);
@@ -28,6 +34,7 @@ void setup() {
   registration.setup();
   gps.setup();
   vessel.setup();
+  load = 0;
 }
 
 void loop() {
@@ -43,6 +50,21 @@ void loop() {
     Serial.print(F("VESSEL: ")); Serial.println( vesselId );
     //vessel.loop( gps.getBearing());
   }
-
+  if ( interrupt.getSecondsFlank()) {
+    interrupt.clearSecondsFlank();
+    load = ( load + 1 ) % 120;
+    int balance = load % REFRESH;
+    //Serial.println( balance );
+    switch ( balance ) {
+      case 0:
+        break;
+      case 1:
+        logger.setup();
+        Serial.println( "LOGGER SETUP COMPLETE" );
+        break;
+      default:
+         break;
+    }
+  }
   delay(1000);
 }
