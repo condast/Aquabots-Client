925d7305418eee916cdfb61431263eb538b4ce43
Condast <info@condast.com>
2019-05-29 21:09:30 +0200

    Added Day7

---
+++ Arduino/Anna/smash/Garmin.h
@@ -0,0 +1,21 @@
+#ifndef Garmin_h
+#define Garmin_h
+
+#include "Arduino.h"
+#include "Vessel.h"
+
+#define GARMIN_ID "bathymetry.garmin.ds70"
+#define GARMIN "GARMIN"
+
+class Garmin {
+
+  public: Garmin(void);
+    void setup();
+    void loop( );
+
+    private: bool enabled;
+};
+
+#endif
+
+
